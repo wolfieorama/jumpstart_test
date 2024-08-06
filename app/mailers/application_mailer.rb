b8a7f942093544e3155da67dec079751fb883e9e
Chris Oliver <excid3@gmail.com>
2024-08-06 15:39:02 -0500

    Move mail configuration to environments instead of initializer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Jumpstart.config.support_email
+  default from: Jumpstart.config.default_from_email
   layout "mailer"
 
   # Include any view helpers from your main app to use in mailers here
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -97,4 +97,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = {host: Jumpstart.config.domain}
+  config.action_mailer.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
 end
