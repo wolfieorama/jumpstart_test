8d71fe8d7c4eed80b86d4ec40a32395d46798fcb
ConfusedVorlon <rob@hobbyistsoftware.com>
2024-09-04 20:08:40 +0100

    Fix errors when no inviter on AccountInvitation (#1210)
    Co-authored-by: Chris Oliver <excid3@gmail.com>

--- app/mailers/account_invitations_mailer.rb
+++ app/mailers/account_invitations_mailer.rb
@@ -7,7 +7,7 @@ class AccountInvitationsMailer < ApplicationMailer
   def invite
     @account_invitation = params[:account_invitation]
     @account = @account_invitation.account
-    @invited_by = @account_invitation.invited_by
+    @invited_by = @account_invitation.invited_by || User.new(name: "Someone")
 
     mail(
       to: email_address_with_name(@account_invitation.email, @account_invitation.name),
